57ea3d20299f60e66031c2fae7b34304f2825856
Sarita <saritasilva368@gmail.com>
2025-08-12 18:54:50 +0100

    amended files so that they were rightly linked

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "3dfefaofbc33edd4c68fae7t3005c4aa";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
@@ -51,4 +51,3 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
-
